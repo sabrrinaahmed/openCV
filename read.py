1960f23f40c598dc3d3d11feba9076bbe58cbd41
sabrrinaahmed <sabrrinaahmed@tamu.edu>
2023-02-12 23:13:16 -0600

    read in photos

---
+++ read.py
@@ -0,0 +1,8 @@
+import cv2 as cv
+
+# Reading pictures
+img = cv.imread('Photos/cat.jpeg') #takes in the path for an image and returns an image as a matrix of pixels
+
+cv.imshow('Cat', img) #displays image as new window, parameters (name of window, matrix of pixels)
+
+cv.waitKey(0) #waits for time in milliseconds for a key to be pressed, 0 = infinite amount of time
