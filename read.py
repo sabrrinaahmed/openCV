5e52dbafc554674cc7b2ac271ea6e86d46101669
sabrrinaahmed <sabrrinaahmed@tamu.edu>
2023-02-12 23:14:18 -0600

    read in photos

--- read.py
+++ read.py
@@ -6,3 +6,5 @@ img = cv.imread('Photos/cat.jpeg') #takes in the path for an image and returns a
 cv.imshow('Cat', img) #displays image as new window, parameters (name of window, matrix of pixels)
 
 cv.waitKey(0) #waits for time in milliseconds for a key to be pressed, 0 = infinite amount of time
+
+#Reading Videos
